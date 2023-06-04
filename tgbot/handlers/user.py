a52a41882578479b68cb7eb8239ea15d7647ff32
Mykhailo Pashchenkoe <bdemlolp234@gmail.com>
2023-06-04 11:34:19 +0300

    bot templete

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,10 +1,14 @@
 from aiogram import Dispatcher
 from aiogram.types import Message
-
+from aiogram.dispatcher.filters import Text
+from tgbot.keyboards.reply import keyboard
 
 async def user_start(message: Message):
     await message.reply("Hello, user!")
 
+async def print(message: Message):
+    await message.reply("Hello World", reply_markup=keyboard)
 
 def register_user(dp: Dispatcher):
     dp.register_message_handler(user_start, commands=["start"], state="*")
+    dp.register_message_handler(print, Text(equals=["negr", "beliy", "billy"]))
